eab66cd4372e305f62224356feaa5885648287ae
Miguel <miasvaleriano@gmail.com>
2019-12-08 12:12:49 -0500

    Added error handling
    Prints out helpful error message when going to other pages

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/:id', (req, res) => {
+app.get('/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = projects.find(({ id }) => id === +projectId);
 
@@ -28,9 +28,22 @@ app.get('/:id', (req, res) => {
         res.render('project', { project });
     }
     else {
-        res.sendStatus(404);
+        console.log("404 Error")
+        const err = new Error('The page was not found');
+        err.status = 404;
+        next(err);
     }
 });
+
+// Updated based on: https://teamtreehouse.com/community/rangeerror-invalid-status-code-undefined-when-linking-to-another-page
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    if (err.status >= 100 && err.status < 600)
+        res.status(err.status);
+    else
+        res.status(500);
+    res.render('error', err);
+})
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
