c430e2bbf93e128c2ed2cb13032aa891409fbc40
Miguel <miasvaleriano@gmail.com>
2019-12-09 14:38:42 -0500

    app.js comments
    Added comments to app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 // REQUIRING files
 const express = require('express');
-//const router = express.Router();
 const { projects } = require('./data.json');
-
-
 
 // SETTING view engine
 const app = express();
 app.set('view engine', 'pug');
 
-// SERVING static
+// SERVING static files to the browser
 app.use(express.static('public'));
 
-// ROUTES
+// ROUTES 
 app.get('/', (req, res) => {
     res.render('index', { projects });
 });
@@ -21,6 +18,7 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// --Routes of each of the projects and then handles 404 error before sending to err middleware
 app.get('/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = projects.find(({ id }) => id === +projectId);
@@ -37,6 +35,7 @@ app.get('/:id', (req, res, next) => {
 });
 
 // Updated based on: https://teamtreehouse.com/community/rangeerror-invalid-status-code-undefined-when-linking-to-another-page
+// Middleware to handle errors such as 404 and 500
 app.use((err, req, res, next) => {
     res.locals.error = err;
     if (err.status >= 100 && err.status < 600)
@@ -46,6 +45,7 @@ app.use((err, req, res, next) => {
     res.render('error', err);
 })
 
+//SET up listening port to 3000
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
 });
