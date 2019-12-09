93a00cf31411eb2bb6d72834306edcb88a5eb744
Miguel <miasvaleriano@gmail.com>
2019-12-08 23:01:24 -0500

    Removed more comments
    Still need to update elevator speech and descriptions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project-6
  Static Node.js and Express Site
+
+ Changes from original--
+ 1) Changed font color for 'My Porfolio' (on style.css)
+ 2) Added random images for 404 Error (error.pug)
+ 3) Changed background color (style.css)
+
+ Rather than use a /projects url, I simply have it so /# (1-5) will take you to the appropriate project
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const { projects } = require('./data.json');
 
 
+
 // SETTING view engine
 const app = express();
 app.set('view engine', 'pug');
@@ -42,8 +43,6 @@ app.use((err, req, res, next) => {
         res.status(err.status);
     else
         res.status(500);
-    //const img404 = Math.floor((Math.random() * 2) + 1);
-
     res.render('error', err);
 })
 
